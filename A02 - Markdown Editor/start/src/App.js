831b3588b2007fbb9f1ee3e6ff8cb1db83e98f1a
phn <pichichi91@users.noreply.github.com>
2021-02-26 23:28:50 +0100

    Add Markdown Editor

--- A02 - Markdown Editor/start/src/App.js
+++ A02 - Markdown Editor/start/src/App.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import marked from "marked"
+import ReactMarkdown from ReactMarkdown;
+
 
 export default function App() {
+
+  const [markdown, setMarkdown] = useState("# sup")
+
+  function handleChange(event) {
+    setMarkdown(event.target.value)
+  }
+
   return (
     <div className="app">
-      <textarea />
-
-      <div className="preview" />
+      <textarea onChange={handleChange} value={markdown} />
+      <ReactMarkdown className="preview" source={markdown} />
     </div>
   );
 }
+
