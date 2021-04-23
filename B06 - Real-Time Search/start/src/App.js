bfe567f4e784a96e58eb1b208c08bd903436c783
Philippe Naegeli <phn@anyweb.ch>
2021-04-23 15:43:05 +0200

    Add Algolia Search

--- B06 - Real-Time Search/start/src/App.js
+++ B06 - Real-Time Search/start/src/App.js
@@ -10,10 +10,42 @@ import './App.css';
 // api key: ae8a3f7cff93d40282a4831930bfa38b
 // index name: products
 
+import algoliasearch from 'algoliasearch/lite';
+import { InstantSearch, SearchBox, Hits, Pagination, Stats } from 'react-instantsearch-dom';
+
+const searchClient = algoliasearch('SSN9CKZUJ2', 'ae8a3f7cff93d40282a4831930bfa38b');
+
+
 export default function App() {
   return (
-    <div className="app">
-      <div className="search-container"></div>
-    </div>
+    <InstantSearch searchClient={searchClient} indexName="products">
+
+      <div className="app">
+        <div className="search-container">
+          <Stats />
+          <SearchBox />
+          <Hits hitComponent={Product} />
+          <Pagination />
+
+        </div>
+
+      </div>
+
+    </InstantSearch>
+
   );
+}
+
+const Product = ({ hit }) => {
+  return (
+    <a className="product" href={hit.url} target="_blank" rel="noopener noreferrer">
+      <img src={hit.image} alt={hit.name} />
+      <div>
+        <h3>{hit.brand}</h3>
+        <h2>{hit.name}</h2>
+        <p>${hit.price}</p>
+      </div>
+    </a>
+  )
+
 }
